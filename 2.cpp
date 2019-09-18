57b968edd6302ac1c245f74aa69392fac0c1972f
aleksma <aleksma99@yandex.ru>
2019-09-18 08:51:49 +0300

    Initial commit

---
+++ 2.cpp
@@ -0,0 +1,42 @@
+// Mamaev Alexey
+
+// Написать программу, генерирующую все подстановки, порожденные
+// произвольной одной подстановкой
+
+
+#include "bits/stdc++.h"
+using namespace std;
+
+bool equal(vector<int> first, vector<int> second) {
+  if (first.size() != second.size())
+    return false;
+
+  for (int i = 0; i < first.size(); ++i) {
+    if (first[i] != second[i])
+      return false;
+  }
+  return true;
+}
+
+int main() {
+
+  int n;
+  cin >> n;
+
+  vector<int> source(n), dest(n), next(n);
+
+  for (int i = 0; i < n; ++i) {
+    cin >> source[i];
+  }
+
+  for (int i = 0; i < n; ++i) {
+    cin >> dest[i];
+    next[i] = dest[i];
+  }
+
+  do {
+
+  } while (!equal(dest, next))
+
+  return 0;
+}
