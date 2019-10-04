257379f75ba59d42fac9d6fd626f93816bc38ac7
aleksma <aleksma99@yandex.ru>
2019-10-04 21:51:24 +0300

    [UPD] -_-

--- HW-0/4_sad.cpp
+++ HW-0/4_sad.cpp
@@ -62,74 +62,65 @@ bool is_assoc() {
 
 // Последовательное порождение некоторым множеством элементов всех возможных значений
 // Породили все элементы - порождающее множество
-bool gen_elements(const set<int> &base, set<int> &elements) {
-  bool get_new = false;
-  set<int> new_elements;
-  for (auto base_elem : base) {
-    for (auto mul : elements) {
-      int left = Matrix[base_elem][mul];
-      if (elements.find(left) == elements.end()) {
-        new_elements.insert(left);
-        get_new = true;
-        continue;
-      }
-      int right = Matrix[mul][base_elem];
-      if (elements.find(right) == elements.end()) {
-        new_elements.insert(right);
-        get_new = true;
-        continue;
+bool is_generator_rec(set<int> elements) {
+  while (true) {
+    bool get_new = false;
+    set<int> new_elements;
+    for (auto elem : elements) {
+      for (auto mul : elements) {
+        int left = Matrix[elem][mul];
+        if (elements.find(left) == elements.end()) {
+          new_elements.insert(left);
+          get_new = true;
+          continue;
+        }
+        int right = Matrix[mul][elem];
+        if (elements.find(right) == elements.end()) {
+          new_elements.insert(right);
+          get_new = true;
+          continue;
+        }
       }
     }
-  }
-  elements.insert(new_elements.begin(), new_elements.end());
-  if (!get_new)
-    return false;
-  else if (elements.size() == N)
-    return true;
-  else {
-    return gen_elements(base, elements);
+    elements.insert(new_elements.begin(), new_elements.end());
+    if (!get_new)
+      return false;
+    else if (elements.size() == N)
+      return true;
   }
 }
 
 // Запуск проверки на порождающее множество
-bool is_generator(set<int> &base) {
-  if (base.size() == N)
+bool is_generator(set<int> &elements) {
+  if (elements.size() == N)
     return true;
-  set<int> elements;
-  elements.insert(base.begin(), base.end());
-  return gen_elements(base, elements);
+  return is_generator_rec(elements);
 }
 
-// Рекурсивная генерация m-местных сочетаний на N элементах с проверкой порождающих
-void generators_rec(const set<int> &base, int m, set<int> comb) {
-  if (m == 0) {
-    bool fl = is_generator(comb);
-    //if (fl) {
-    for (auto t : comb) {
-      //cout << get_elem(t) << " ";
-      cout << t << " ";
+void make_generators_set(int k) {
+  int j = 0;
+  set<int> generators;
+  while (j < N) {
+    if (1 << j & k) {
+      generators.insert(j);
+    }
+    j++;
+  }
+  if (is_generator(generators)) {
+    for (auto t : generators) {
+      cout << get_elem(t) << " ";
     }
     cout << endl;
-    found_generators = true;
-    //}
-  } else {
-    set<int> base_new;
-    base_new.insert(base.begin(), base.end());
-    for (auto q : base) {
-      if (base.size() < m)
-        return;
-      base_new.erase(q);
-      set<int> comb_new;
-      comb_new.insert(comb.begin(), comb.end());
-      comb_new.insert(q);
-      generators_rec(base_new, m - 1, comb_new);
-    }
   }
 }
 
-void generators(const set<int> &base, int m) {
-  set<int> c;
-  generators_rec(base, m, c);
+void make_all_combs(vector<int> &base) {
+  cout << "Порождающие:\n";
+  int k = 1, max = 1 << N;
+  while (k < max) {
+    make_generators_set(k);
+    k++;
+  }
 }
 
 bool is_left_e(int elem) {
@@ -151,15 +142,11 @@ bool is_right_e(int elem) {
 int main() {
   is_assoc();
 
-  set<int> base;
+  vector<int> base(N);
   for (int i = 0; i < N; ++i)
-    base.insert(i);
+    base[i] = i;
 
-  int m = 1;
-  cout << "Порождающие:\n";
-  //while (!found_generators)
-  while (m < 4)
-    generators(base, m++);
+  make_all_combs(base);
 
   cout << endl;
 
